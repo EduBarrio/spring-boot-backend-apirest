be8cd8a3a8f5d97e4dc39feef6896f2c3f47a25c
Eduardo Barrio <edubp89@gmail.com>
2020-04-13 12:51:47 +0100

    First commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Edu', 'Barrio', 'edu@gmail.com', '2020-03-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Ada', 'Peña', 'ada@gmail.com', '2020-03-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Pedro', 'Suarez', 'peter@gmail.com', '2020-03-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Marcos', 'Aldani', 'marquito@gmail.com', '2020-03-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Rigoberto', 'Florencia', 'rigo@gmail.com', '2020-03-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Mingorance', 'Barreto', 'mingo@gmail.com', '2020-03-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Aythami', 'Suárez', 'aytha@gmail.com', '2020-03-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Josué', 'Cabello', 'josu@gmail.com', '2020-03-01');
